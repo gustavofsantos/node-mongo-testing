d35a2e98ee85476fc4ad17855dd270e895d7b102
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-19 12:35:22 -0300

    read data from database

--- app/routers/users-router.js
+++ app/routers/users-router.js
@@ -3,8 +3,15 @@ const { connect } = require("../../db/mongo");
 async function usersRouter(fastify) {
   const db = await connect();
 
-  fastify.get("/users", (req, reply) => {
-    const users = db.collection("users").find({}).toArray();
+  await db.collection("users").deleteMany({});
+  await db.collection("users").insertMany([
+    { name: "João", email: "joao@email.com" },
+    { name: "Maria", email: "maria@email.com" },
+  ]);
+
+  fastify.get("/users", async (req, reply) => {
+    const users = await db.collection("users").find({}).toArray();
+
     reply.send(users);
   });
 }
