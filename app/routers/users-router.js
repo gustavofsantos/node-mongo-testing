feace97252972207b65275660e67afa3418565f9
Gustavo Santos <gustavofsantos@outlook.com>
2020-12-19 15:35:49 -0300

    use user service in the router

--- app/routers/users-router.js
+++ app/routers/users-router.js
@@ -1,11 +1,11 @@
-const { connect } = require("../../db/mongo");
+const { UserService } = require("../services/user-service");
 
 async function usersRouter(fastify) {
-  const db = await connect();
+  const userService = new UserService();
+  await userService.connect();
 
   fastify.get("/users", async (req, reply) => {
-    const users = await db.collection("users").find({}).toArray();
-
+    const users = await userService.listAll();
     reply.send(users);
   });
 }
